a3fccc041c027d18c7387a95a2a014e1f5acc572
Roslyn10 <roslynclewis1004@gmail.com>
2024-10-18 16:19:47 +0200

    Adding the server code for the quizz and the dictionary containing the questions and answers for the Introdctuion module

---
+++ quizzes/Introduction_quiz.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" A quiz dictionary based off the Introduction module"""
+
+quiz_questions = [
+        {
+            "question": "What is the internet?",
+            "options": [1. "A global network of computers", 2. "A website that contains information", 3.. "The World Wide Web", 4. "A collection of websites we want to view"]
+            "answer": "1"
+        },
+        {
+            "question": "What is the difference between the internet and the World Wide Web",
+            "options": [1. "The internet is a global network that connects computers, and the web is a collection of information accessed through this network", 2. "The World Wide Web is the internet itself, they are the same thing"]
+            "answer": "1"
+            
+        },
+        {
+        "quesion": "Which one is not one of the different types of internt connections?"
+        "options": [1. "Dial-up", 2. "DSL", 3. "Cable", 4. "Fiber", 5. "Cellulatr"]
+        "answer": "2"
+        },
+        {
+            "question": "What is an IP address?",
+            "options": [1. "A unique identifier for a device connected to the nternet", 2. "A number used by wensites to store data", 3. "The location of a computer's data on the internt", 4. "A code that encrypts information on the web"]
+            "answer": "1"
+        },
+        {
+            "questions": "What is a server used for?",
+            "options": [1. "To store and deliver data(like websites) to users on the internet", 2. "To access the World Wide Web", 3. "To control the entire internet", 4. "To store user's personal information"
+            "answer": "1"
+        },
+] 
