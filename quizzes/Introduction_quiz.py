daa144bc40d61d57519d730ce461ce4e01c69a72
Roslyn10 <roslynclewis1004@gmail.com>
2024-10-18 16:53:18 +0200

    Fixing the syntax of the introducation qquiz dictionary

--- quizzes/Introduction_quiz.py
+++ quizzes/Introduction_quiz.py
@@ -4,28 +4,28 @@
 quiz_questions = [
         {
             "question": "What is the internet?",
-            "options": [1. "A global network of computers", 2. "A website that contains information", 3. "The World Wide Web", 4. "A collection of websites we want to view"],
-            "answer": "1"
+            "options": ["A global network of computers", "A website that contains information", "The World Wide Web", "A collection of websites we want to view"],
+            "answer": "A global network of computers"
         },
         {
             "question": "What is the difference between the internet and the World Wide Web",
-            "options": [1. "The internet is a global network that connects computers, and the web is a collection of information accessed through this network", 2. "The World Wide Web is the internet itself, they are the same thing"],
-            "answer": "1"
+            "options": ["The internet is a global network that connects computers, and the web is a collection of information accessed through this network","The World Wide Web is the internet itself, they are the same thing"],
+            "answer": "Th internet is a global network that connects computers, ans the web is a collection of information accessed through this network"
             
         },
         {
         "quesion": "Which one is not one of the different types of internt connections?"
-        "options": [1. "Dial-up", 2. "DSL", 3. "Cable", 4. "Fiber", 5. "Cellulatr"],
-        "answer": "2"
+        "options": ["Dial-up", "DSL", "Cable", "Fiber", "Cellulatr"],
+        "answer": "DSL"
         },
         {
             "question": "What is an IP address?",
-            "options": [1. "A unique identifier for a device connected to the nternet", 2. "A number used by wensites to store data", 3. "The location of a computer's data on the internt", 4. "A code that encrypts information on the web"],
-            "answer": "1"
+            "options": ["A unique identifier for a device connected to the internet", "A number used by wensites to store data", "The location of a computer's data on the internt", "A code that encrypts information on the web"],
+            "answer": "A unique identifier for a device connected to the internet"
         },
         {
             "questions": "What is a server used for?",
-            "options": [1. "To store and deliver data(like websites) to users on the internet", 2. "To access the World Wide Web", 3. "To control the entire internet", 4. "To store user's personal information"],
+            "options": ["To store and deliver data(like websites) to users on the internet", "To access the World Wide Web", "To control the entire internet", "To store user's personal information"],
             "answer": "1"
         },
 ] 
