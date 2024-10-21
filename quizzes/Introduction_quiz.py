4a690b87512702e2e2edc6b8f763d7d86c164cc9
Roslyn10 <roslynclewis1004@gmail.com>
2024-10-21 20:29:12 +0200

    Fixing the error in the quiz dictionary

--- quizzes/Introduction_quiz.py
+++ quizzes/Introduction_quiz.py
@@ -4,28 +4,28 @@
 quiz_questions = [
         {
             "question": "What is the internet?",
-            "options": ["A. A global network of computers B. A website that contains information, C. The World Wide Web, D. A collection of websites we want to view"],
-            "answer": "A."
+            "options": ["A global network of computers", " A website that contains information", " The World Wide Web", " A collection of websites we want to view"],
+            "answer": "1"
         },
         {
             "question": "What is the difference between the internet and the World Wide Web",
-            "options": ["A. The internet is a global network that connects computers, and the web is a collection of information accessed through this network","B. The World Wide Web is the internet itself, they are the same thing"],
-            "answer": "A."
+            "options": ["The internet is a global network that connects computers, and the web is a collection of information accessed through this network","The World Wide Web is the internet itself, they are the same thing"],
+            "answer": "2"
             
         },
         {
         "quesion": "Which one is not one of the different types of internt connections?",
-        "options": ["A. Dial-up", "B. DSL", "C. Cable", "D. Fiber", "E. Cellulatr"],
-        "answer": "B."
+        "options": ["A. Dial-up", "B. DSL", "C. Cable", "D. Fiber", "E. Cellular"],
+        "answer": "2"
         },
         {
             "question": "What is an IP address?",
             "options": ["A. A unique identifier for a device connected to the internet", "B. A number used by wensites to store data", "C. The location of a computer's data on the internt", "D. A code that encrypts information on the web"],
-            "answer": "A."
+            "answer": "1"
         },
         {
             "questions": "What is a server used for?",
             "options": ["A. To store and deliver data(like websites) to users on the internet", "B. To access the World Wide Web", "C. To control the entire internet", "D. To store user's personal information"],
-            "answer": "A."
+            "answer": "1"
         },
 ] 
--- quizzes/quiz_data.py
+++ quizzes/quiz_data.py
@@ -2,23 +2,23 @@
 
 quiz_questions = [
     {
-        "question": "What is the capital of France?",
-        "options": ["Berlin"," Madrid"," Paris", "Rome"],
+        "question": "What is the internet?",
+        "options": ["A global network of computers","A website that contains information","The World Wide Web", "A collection of websites we want to view"],
+        "answer": "1"
+    },
+    {
+        "question": "What is the difference between the internet and the world wide web?",
+        "options": ["The World Wide Web is the internet itself","The internet is a glabal network that connect computers, and the web is a collection of information accessed through this network"],
+        "answer": "2"
+    },
+    {
+        "question": "What is an IP address?",
+        "options": ["A code that encrypts information on the web", "The location of a computer's data on the internet", "A unique identifier for a device connected to the internt", "A number used by websited to store data"],
         "answer": "3"
-    },
-    {
-        "question": "What is 2 + 2?",
-        "options": ["3", "4", "5", "6"],
-        "answer": "2"
-    },
-    {
-        "question": "Which planet in the solar system is the hottest?",
-        "options": ["Mercury", "Venus", "Earth", "Mars"],
-        "answer": "2"
         },
     {
-        "question": "What is the most abundant gas in Earth's atmosphere?",
-        "options": ["Nitrogen", "Oxygen", "Carbon-Dioxide", "Hydrogen"],
-        "answer": "1"
+        "question": "What is a server used for?",
+        "options": ["To store user's personal information", "To control the entire internet", "To access the Worl Wide Web", "To store and deliver data(like websites) to users on the internet"],
+        "answer": "4"
         }
 ]
