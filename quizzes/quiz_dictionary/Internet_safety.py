9ef0fcf594e0c01d7f8727be25a74e42e37510dd
Roslyn Lewis <139125575+Roslyn10@users.noreply.github.com>
2024-10-24 21:11:45 +0200

    Fixing the reference to the css files in the index file and adding the first half of the questions in the internet safety dictionary

---
+++ quizzes/quiz_dictionary/Internet_safety.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+quiz_question = [
+    {
+        "question": "Which of these is the safest password?",
+        "options": ["Qwerty42890", "password#12", "Vb3yiwenldsvn824q", "123455678"], 
+        "answer": "3"
+    },
+    {
+        "Which website is more secure?",
+        "options": ["http://www.getinfo.com", "https://www.moreinfor.com"],
+        "answer": "2"
+    },
+    {
+        "question": "What should you check before entering your credit details on a website?",
+        "options": ["The site has a "Contact us" page", "The URL starts with https", "The website has pictures of the prouct", "The website offers free shipping"],
+        "answer": "2"
+    },
+    {
+        "question": "Which action is good internet safety practice?",
+        "options": ["Sharing your password with friends and family", "Clicking on links in an email from someone you don't know", "Using public Wi-Fi to check your bacnk account", "Logging out/Signing out of your account after using a public device"], 
+        "answer": "4"
+    },
+    {
+        "question": "What shouold you doif you receive an email from someone you don't know asking for personal information?",
+        "options": ["Reply with the infromation", "Ignore and delete the email", "Click the link in the email to learn more", "Share the email with your friends and family"],
+        "answer": "2"
+    },
+    {
+        "question": "What is a common online threat related to social media?",
+        "options": ["Data breaches", "Malware and viruses", "Romance scams", "Phishing emails"],
+        "answer": "3"
+    },
+
+]
