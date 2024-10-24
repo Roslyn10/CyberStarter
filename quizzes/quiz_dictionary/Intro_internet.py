3ebf65c9a24fa5ac4e3fd33a30271bea1bfe948f
Roslyn Lewis <139125575+Roslyn10@users.noreply.github.com>
2024-10-24 19:17:49 +0200

    Adding the quiz questions for the first module

---
+++ quizzes/quiz_dictionary/Intro_internet.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3 
+
+quiz_questions = [
+    {
+        "question": "What is the internet?",
+        "options": ["A website that contains information", "The World Wide Web", "A collection of websites we want to view", "A global network of computers"]
+        "answer": "1"
+    },
+    {
+        "question": "What is the main difference between the internet and the World Wide Web?"
+        "options": ["The internet is the global network that connects computers, and the web is a collection of information accessed through this network", "The World Wide Web is the internet itself, and there is no difference"]
+        "answer": "1"
+    },
+    {
+        "question": "What are the different types of internet connections?"
+        "options": ["Dial-up, DSL, Cable, Fiber, Cellular", "Server, Data, byte, archie, LAN", "Modem, net, network, search engine, spam", "Dial-up, DSL, TCP, IP, CERN, bandwidth", "ASCII, zine, website, word, Fiber"]
+        "answer": "1"
+    },
+    {
+        "question": "What is an IP address?",
+        "options": ["A numberused by websites to store data", "A unique identifier for a device connected to the internet", "The location of a computer's data on the internet", "A code the encrypts information on the web"],
+        "answer": "2"
+    },
+    {
+        "question": "What is a server used for?",
+        "options": ["To access the World Wide Web", "To control the entire internet", "To store and deliver data (like websites) to users on the internt", "To store users' personal information"],
+        "answer": "3"
+    },
+    {
+        "question": "What is the role of an ISP (Internet Service Provider)?",
+        "options": ["To manage all internet content", "To store user data securely", "To provide access to the internet", "To provide security for online transactions"],
+        "answer": "3"
+    },
+    {
+        "question": "Which is the following best describes dial-up internet?",
+        "options": ["An internet service that connects through cable TV", "A wireless connection that does not use any cables", "A high-speed connection using fiber optics", "An older, slower connection that uses phone lines"],
+        "answer": "4"
+    },
+    {
+        "question": "What does the domain Name System (DNS) do?",
+        "options": ["Manages email services for users", "Translates website addresses into IP addresses", "Provides internet security features", "Stores website data for fast access"],
+        "answer": "2"
+    },
+    {
+        "question": "How does data travel across the internet?",
+        "options": ["Only via wirless connections", "Through physical devices only", "In pakcets that are reassembles at the destination", "As a continous stream without interruption"],
+        "answer": "3"
+    },
+    {
+        "question": "What is the Transmission Control Protocol (TCP)?",
+        "options": ["A set of rules for ensuring smooth data transmission", "A type of internet connection method", "A way to encrypt user data", "A program for managing web servers"],
+        "answer": "1"
+    },
+]
