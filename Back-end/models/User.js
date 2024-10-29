00e7d4bbf2f355347f941250906129d063775fc6
Roslyn10 <roslynclewis1004@gmail.com>
2024-10-29 12:52:34 +0200

    Removing the node_modules and re-installing it after installing bcryptjs

--- Back-end/models/User.js
+++ Back-end/models/User.js
@@ -2,20 +2,27 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-	username: { type: String, required: true, unique: true},
-	email: { type: String, required: true, unique: true},
-	password: { type: String, required: true}
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
 });
 
+// Hash the password before saving the user
 userSchema.pre('save', async function(next) {
-	if (!this.isModified('password')) return next();
-	const salt = await bcrypt.gensalt(10);
-	this.password = await bcrypt.hash(this.password, salt);
-	next();
+    if (!this.isModified('password')) return next();
+    try {
+        const salt = await bcrypt.genSalt(10); // Generate salt
+        this.password = await bcrypt.hash(this.password, salt); // Hash the password
+        next();
+    } catch (error) {
+        next(error); // Pass the error to the next middleware
+    }
 });
 
+// Method to compare password
 userSchema.methods.matchPassword = async function(password) {
-	return await bcrypt.compare(password, this.password);
+    return await bcrypt.compare(password, this.password);
 };
 
 module.exports = mongoose.model('User', userSchema);
+
