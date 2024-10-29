0147cd2a0b5310b209e0a9638556287d0b869048
Roslyn10 <roslynclewis1004@gmail.com>
2024-10-29 13:44:16 +0200

    Fixing some typos and errors and the rest is running properly

--- Back-end/controllers/AuthController.js
+++ Back-end/controllers/AuthController.js
@@ -1,6 +1,11 @@
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 
+// Function to generate JWT
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+};
+
 // User Registration
 exports.register = async (req, res) => {
     const { username, email, password } = req.body;
@@ -20,6 +25,7 @@ exports.register = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
 // User Login
 exports.login = async (req, res) => {
     const { email, password } = req.body;
@@ -43,7 +49,3 @@ exports.login = async (req, res) => {
     }
 };
 
-// Function to generate JWT
-const generateToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '1d' });
-};
