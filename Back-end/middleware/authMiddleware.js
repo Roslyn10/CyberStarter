e847ced37f71ae91ebe24f82c012081ddb58174c
Roslyn10 <roslynclewis1004@gmail.com>
2024-10-30 15:44:14 +0200

    Fixing a spelling error in the authMIddleware from decode to decoded

--- Back-end/middleware/authMiddleware.js
+++ Back-end/middleware/authMiddleware.js
@@ -7,7 +7,7 @@ const protect = async (req, res, next) => {
 		token = req.headers.authorization.split(' ')[1];
 
 		try {
-			const decode = jwt.verify(token, process.env.JWT_SECRET);
+			const decoded = jwt.verify(token, process.env.JWT_SECRET);
 			req.user = await User.findById(decoded.id).select('-password');
 			next();
 		} catch (error) {
