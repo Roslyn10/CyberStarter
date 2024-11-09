f70bc269dc8a43e0a558a07fb0ba95cd0303b11a
Roslyn10 <roslynclewis1004@gmail.com>
2024-11-09 20:19:48 +0200

    Adding in a section of code that allows the user to be able to edit their profile

--- Back-end/routes/auth.js
+++ Back-end/routes/auth.js
@@ -22,4 +22,32 @@ router.get('/profile', auth, async (req, res) => {
     }
 });
 
+// Update Profile route with authentication middleware
+router.put('/profile', auth, async (req, res) => {
+    const { username, email, password } = req.body;
+
+    try {
+        const user = await User.findById(req.user.id);
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        // Update fields only if provided
+        if (username) user.username = username;
+        if (email) user.email = email;
+        if (password) {
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password, salt);
+        }
+
+        await user.save();
+
+        // Return the updated profile
+        res.json({ username: user.username, email: user.email });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+
 module.exports = router;
