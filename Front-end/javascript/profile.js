4da1fd2045297b2527fb861e8f6ffff09d3bba37
Roslyn10 <roslynclewis1004@gmail.com>
2024-11-11 15:23:30 +0200

    Fixing the referance to the logout button

--- Front-end/javascript/profile.js
+++ Front-end/javascript/profile.js
@@ -39,6 +39,6 @@ document.addEventListener('DOMContentLoaded', async () => {
 
 // Logout functionality
 document.getElementById('logout-btn').addEventListener('click', () => {
-  localStorage.removeItem('token'); 
-  window.location.href = 'login.html'; 
+  localStorage.removeItem('token');
+  window.location.href = 'login-page.html';
 });
