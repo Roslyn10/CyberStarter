d3a27f62899e740ddfb5621f5c8059df07a53204
Roslyn10 <roslynclewis1004@gmail.com>
2024-11-12 21:56:43 +0200

    Fixing the semistandard for the authMiddleware and the auth files

--- Back-end/middleware/auth.js
+++ Back-end/middleware/auth.js
@@ -2,22 +2,22 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
-    // Retrieve token from headers
-    const token = req.header('Authorization').replace('Bearer ', '');
+  // Retrieve token from headers
+  const token = req.header('Authorization').replace('Bearer ', '');
 
-    // Check if no token is provided
-    if (!token) {
-        return res.status(401).json({ message: 'No token, authorization denied' });
-    }
+  // Check if no token is provided
+  if (!token) {
+    return res.status(401).json({ message: 'No token, authorization denied' });
+  }
 
-    try {
-        // Verify token and get user ID
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded.user;
-	console.log("Decoded User:", req.user); // Logs the decoded user 
-        next();
-    } catch (err) {
-	console.log("Token Verification Falied:", err);
-        res.status(401).json({ message: 'Token is not valid' });
-    }
+  try {
+    // Verify token and get user ID
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.user;
+    console.log('Decoded User:', req.user); // Logs the decoded user
+    next();
+  } catch (err) {
+    console.log('Token Verification Falied:', err);
+    res.status(401).json({ message: 'Token is not valid' });
+  }
 };
